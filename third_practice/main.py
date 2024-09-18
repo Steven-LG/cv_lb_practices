d6e7d3d35062a0c92ee9a890d9c4bee681806d16
Steven Luna <100967107+Steven-LG@users.noreply.github.com>
2024-09-18 01:11:44 -0600

    feat: original data set for second practice working

--- third_practice/main.py
+++ third_practice/main.py
@@ -18,7 +18,6 @@ def save_channel_to_csv(channel, filename, fmt='%.8f'):
     filepath = os.path.join(csv_output_folder, filename)
     np.savetxt(filepath, channel, delimiter=',', fmt=fmt)
 
-
 image = cv2.imread(f'{base_practice_folder}/image.png')
 segment_height = 40
 segment_width = 62
@@ -28,64 +27,81 @@ start_row, start_col = 100, 150
 end_row = start_row + segment_height
 end_col = start_col + segment_width
 
-image_segment = cv2.cvtColor(image[start_row:end_row, start_col:end_col], cv2.COLOR_BGR2GRAY)
-cv2.imshow('Original Segment', cv2.resize(image_segment, (segment_width * 10, segment_height * 10), interpolation=cv2.INTER_NEAREST))
+# image_segment = cv2.cvtColor(image[start_row:end_row, start_col:end_col], cv2.COLOR_BGR2GRAY)
+
+image_segment = np.array([
+    [232, 224, 131, 232, 233],
+    [215, 231, 234, 230, 118],
+    [230, 218, 226, 219, 226],
+    [215, 106, 228, 230, 232],
+    [224, 221, 234, 226, 217],
+])
+
 save_channel_to_csv(image_segment, 'segmento_original.csv', fmt='%d')
 
-print(np.array(image_segment))
+# print(np.array(image_segment))
 
 median = np.median(image_segment)
-print(f'Median: {median} \n')
-mean, std_dev = cv2.meanStdDev(image_segment)
-mean = mean[0][0]
-threshold = 1.5
-print(f'Mean: {mean} \n')
+regional_intensity_mean = np.mean(image_segment)
+mode = stats.mode(image_segment, axis=None).mode
 
-mode_value = stats.mode(image_segment, axis=None).mode
-print(f'Mode: {mode_value} \n')
+print(f'Median: {median}')
+print(f'Mean: {regional_intensity_mean}')
+print(f'Mode: {mode}')
 
-deviations_percent = np.abs((mean - image_segment * 100) / mean)
+threshold = 10
+# threshold = 75
 
-percentile_threshold = np.percentile(deviations_percent, 95)
-print(f'Percentil 95: {percentile_threshold}')
-
-print("Matriz de desviaciones:")
-print(deviations_percent)
-save_channel_to_csv(deviations_percent, 'desviaciones.csv')
-
-anomalies = deviations_percent > threshold
-print("Matriz de anomalias:")
-print(anomalies)
-save_channel_to_csv(anomalies, 'anomalias.csv', fmt='%d')
-
-# use_mean_filter = 1
-filtered_segment = np.copy(image_segment)
-for i in range(image_segment.shape[0]):
-    for j in range(image_segment.shape[1]):
-        if filtered_segment[i, j] > percentile_threshold:
-            # if use_mean_filter == 1:
-            filtered_segment[i, j] = median
-            # else:
-            #     filtered_segment[i, j] = mode_value
+percentual_deviations = np.abs((image_segment - regional_intensity_mean) * 100 / regional_intensity_mean)
+sum_percentual_deviations = np.sum(percentual_deviations)
 
 
-# filtered_segment[anomalies] = median
 
-print(filtered_segment)
-cv2.imshow('Image with filtered segment', cv2.resize(filtered_segment, (segment_width * 10, segment_height * 10), interpolation=cv2.INTER_NEAREST))
+# min_intensity = np.min(percentual_deviations)
+# max_intensity = np.max(percentual_deviations)
 
-counts, bins, patches = plt.hist(deviations_percent.ravel(), bins=50, color='blue', alpha=0.7)
-# plt.xticks(bins, rotation=90)
+# bins = np.arange(min_intensity, max_intensity + 2) - 0.5
+counts, bins, patches = plt.hist(percentual_deviations.ravel(), bins=50, color='blue', alpha=0.7)
+plt.xlim([50, 100])
+
+# plt.xticks(np.arange(min_intensity, max_intensity + 1), rotation=0)
 plt.title('Histograma de Desviaciones Porcentuales')
 plt.xlabel('Desviación Porcentual')
 plt.ylabel('Frecuencia')
 plt.savefig(f'{histogram_output_folder}/segment_deviations.png')
 
-# plt.hist(image_segment.ravel(), bins=256, range=[0, 256], color='gray', alpha=0.7)
-# plt.title('Histograma de intensidades de píxeles')
-# plt.xlabel('Intensidad de píxeles')
+# counts, bins, patches = plt.hist(percentual_deviations.ravel(), bins=percentual_deviations.size, color='blue', alpha=0.7)
+# plt.xticks(bins, rotation=90)
+# plt.title('Histograma de Desviaciones Porcentuales')
+# plt.xlabel('Desviación Porcentual')
 # plt.ylabel('Frecuencia')
-# plt.savefig(f'{histogram_output_folder}/segment_intentisies.png')
+# plt.savefig(f'{histogram_output_folder}/segment_deviations.png')
+
+anomalies = percentual_deviations > threshold
+# save_channel_to_csv(anomalies, 'anomalias.csv', fmt='%d')
+
+
+anomalies_rgb = np.zeros((*anomalies.shape, 3), dtype=np.uint8)
+anomalies_rgb[anomalies] = [0, 0, 255]  # Rojo
+anomalies_rgb[~anomalies] = [0, 0, 0]   # Negro
+
+cv2.imshow('Anomalies', anomalies_rgb)
+
+median_filtered_segment = np.copy(image_segment)
+median_filtered_segment[anomalies] = median
+median_filtered_regional_intensity_mean = np.mean(median_filtered_segment)
+median_filtered_percentual_deviations = np.abs((median_filtered_segment - median_filtered_regional_intensity_mean) * 100 / median_filtered_regional_intensity_mean)
+
+mode_filtered_segment = np.copy(image_segment)
+mode_filtered_segment[anomalies] = mode
+mode_filtered_regional_intensity_mean = np.mean(mode_filtered_segment)
+mode_filtered_percentual_deviations = np.abs((mode_filtered_segment - mode_filtered_regional_intensity_mean) * 100 / mode_filtered_regional_intensity_mean)
+
+
+cv2.imshow('Original segment', image_segment.astype(np.uint8))
+cv2.imshow('Median filtered segment', median_filtered_segment.astype(np.uint8))
+cv2.imshow('Mode filtered segment', mode_filtered_segment.astype(np.uint8))
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
