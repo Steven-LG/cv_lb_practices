f0038dbbb4c2d50e7944b12819b1fdc217866c16
Steven Luna <100967107+Steven-LG@users.noreply.github.com>
2024-09-17 23:41:05 -0600

    feat: thresholding in 3, 4, 8 and 16 tones

--- fourth_practice/main.py
+++ fourth_practice/main.py
@@ -28,15 +28,15 @@ start_row, start_col = 100, 150
 end_row = start_row + segment_height
 end_col = start_col + segment_width
 
-# intensity_matrix = cv2.cvtColor(image[start_row:end_row, start_col:end_col], cv2.COLOR_BGR2GRAY)
-intensity_matrix = np.array([
-    [153, 175, 160, 176, 159, 149],
-    [167, 163, 158, 155, 162, 166],
-    [171, 171, 153, 153, 179, 159],
-    [167, 165, 163, 176, 152, 170],
-    [160, 152, 179, 151, 179, 147],
-    [158, 173, 176, 162, 167, 157],
-])
+intensity_matrix = cv2.cvtColor(image[0:480, 0:720], cv2.COLOR_BGR2GRAY)
+# intensity_matrix = np.array([
+#     [153, 175, 160, 176, 159, 149],
+#     [167, 163, 158, 155, 162, 166],
+#     [171, 171, 153, 153, 179, 159],
+#     [167, 165, 163, 176, 152, 170],
+#     [160, 152, 179, 151, 179, 147],
+#     [158, 173, 176, 162, 167, 157],
+# ])
 
 sorted_normalized_intensity_matrix = np.sort(intensity_matrix, axis=None).reshape(intensity_matrix.shape)
 unique_sorted_normalized_intensity_matrix = np.unique(sorted_normalized_intensity_matrix)
@@ -170,12 +170,71 @@ def find_optimal_threshold_min_entropy(intensities):
         
     return optimal_threshold, min_entropy    
 
-optimal_threshold, min_variance_relation = find_optimal_threshold(sorted_normalized_intensity_matrix=sorted_normalized_intensity_matrix)
-print(f'Optimal threshold {optimal_threshold}')
-print(f'Min variance relation {min_variance_relation}')
+def calculate_global_valley(unique_values, unique_probabilities, image_mean):
+    image_variance = (unique_values-image_mean)**2 * unique_probabilities
+    standard_deviation = np.sqrt(np.sum(image_variance))
+
+    print(f'First: {1}')
+    print(f'Second: {unique_frequencies.shape[0]-1}')
+
+    max_value = np.array([])
+    best_threshold = 0
+    for value, index in enumerate(range(0, unique_frequencies.shape[0]-2)):
+        # print(f'{value} - {index}')
+        # print(f'{unique_values[index+1]}')
+        x = ((standard_deviation*(unique_frequencies[index]-unique_frequencies[index+1])) + (standard_deviation*(unique_frequencies[index+2]-unique_frequencies[index+1]))) / 2    
+        # print(f'Primer valor: {unique_frequencies[index]}')
+        # print(f'Segundo valor: {unique_frequencies[index+1]}')    
+        # print(f'This is x: {x}')
+
+        if max_value.size == 0 or np.abs(x) > np.max(max_value):
+            best_threshold = unique_values[index+1]
+        
+        max_value = np.append(max_value, np.abs(x))
+        # print(f'Best threshold: {best_threshold}')
+
+    return (np.max(max_value), best_threshold)
+
+# optimal_threshold, min_variance_relation = find_optimal_threshold(sorted_normalized_intensity_matrix=sorted_normalized_intensity_matrix)
+# print(f'Optimal threshold {optimal_threshold}')
+# print(f'Min variance relation {min_variance_relation}')
 
 # calculate_variance_relation(image_mean=image_mean, unique_probabilities=unique_probabilities, unique_values=unique_values, intensity_threshold=162)
-optimal_threshold, min_entropy = find_optimal_threshold_min_entropy(intensities=unique_values)
-print(f'Entropía: {calculate_total_entropy()}')
-print(f'Entropía minima optima: {optimal_threshold}')
-print(f'Entropía minima: {min_entropy}')
+# optimal_threshold, min_entropy = find_optimal_threshold_min_entropy(intensities=unique_values)
+# print(f'Entropía: {calculate_total_entropy()}')
+# print(f'Entropía minima optima: {optimal_threshold}')
+# print(f'Entropía minima: {min_entropy}')
+
+# max_value, best_threshold = calculate_global_valley(unique_values, unique_probabilities, image_mean)
+# print(f'Max value: {max_value}')
+# print(f'Best threshold: {best_threshold}')
+
+
+def reduce_tones(image, num_tones):
+    thresholds = np.linspace(0, 256, num_tones + 1)
+    reduced_image = np.zeros_like(image)
+
+    for i in range(num_tones):
+        lower_bound = thresholds[i]
+        upper_bound = thresholds[i + 1]
+
+        print(f'Lower bound: {lower_bound}')
+        print(f'Upper bound: {upper_bound}')
+
+        mask = (image >= lower_bound) & (image < upper_bound)
+        reduced_image[mask] = (lower_bound + upper_bound) // 2
+
+    return reduced_image
+
+
+three_tone_img = reduce_tones(intensity_matrix, 3).astype(np.uint8)
+four_tone_img = reduce_tones(intensity_matrix, 4).astype(np.uint8)
+eight_tone_img = reduce_tones(intensity_matrix, 8).astype(np.uint8)
+sixteen_tone_img = reduce_tones(intensity_matrix, 16).astype(np.uint8)
+
+cv2.imshow('3 tone', three_tone_img)
+cv2.imshow('4 tone', four_tone_img)
+cv2.imshow('8 tone', eight_tone_img)
+cv2.imshow('16 tone', sixteen_tone_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
